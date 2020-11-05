c29b1a1563eed4b3c411b1f25392b10f8a42a9b8
azul-de-prusia <tegthyn_600@hotmail.com>
2020-11-05 12:35:53 -0600

    Updates

---
+++ precache-manifest.32d446084059966b7c08ce0df8b7e819.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dff9a1e5a0f1294b2f7bdb62d7b828cb",
+    "url": "/draw-portfolio/index.html"
+  },
+  {
+    "revision": "0b85d7d5a66e8668ddb9",
+    "url": "/draw-portfolio/static/css/main.95f218bd.chunk.css"
+  },
+  {
+    "revision": "71473069b883bacafba8",
+    "url": "/draw-portfolio/static/js/2.e972b8f7.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/draw-portfolio/static/js/2.e972b8f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b85d7d5a66e8668ddb9",
+    "url": "/draw-portfolio/static/js/main.8314d53e.chunk.js"
+  },
+  {
+    "revision": "d1a9981a8c6c333b5058",
+    "url": "/draw-portfolio/static/js/runtime-main.c3900315.js"
+  }
+]);
